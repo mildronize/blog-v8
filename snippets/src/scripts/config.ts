d5788b895e31fbb8264faf580a4d3f527091a50b
Thada Wangthammang <thada.wth@gmail.com>
2024-12-13 22:49:32 +0700

    refactor: config into file

---
+++ snippets/src/scripts/config.ts
@@ -0,0 +1,11 @@
+
+export const config = {
+  /**
+   * This module is responsible for generating the blog id mapper.
+   */
+  blogIdModule: {
+    sourceDirectories: ["../content/posts"],
+    targetFile: "../public/api/id-mapper.json",
+    ignoreMarkdownFiles: ["_index.md"],
+  }
+}
--- snippets/src/scripts/generate-id-mapper.ts
+++ snippets/src/scripts/generate-id-mapper.ts
@@ -2,18 +2,16 @@ import fs from "fs-extra";
 import path from "path";
 
 import { MarkdownFileProcessor, processMarkdownDirectories } from "./libs/markdown-files";
+import { config } from "./config";
 
-const IGNORE_MARKDOWN_FILES = ["_index.md"];
+const { sourceDirectories, targetFile, ignoreMarkdownFiles } = config.blogIdModule;
 
 console.log("Generating ID mapper...");
 
-const sourceDirectories = ["../content/posts"];
-const targetDirectory = "../public/api";
-
-const processor = new MarkdownFileProcessor(IGNORE_MARKDOWN_FILES);
+const processor = new MarkdownFileProcessor(ignoreMarkdownFiles);
 const idMapperCollection = await processMarkdownDirectories(sourceDirectories, processor) ?? new Map();
 // Ensure target directory exists
-await fs.ensureDir(targetDirectory);
-fs.writeJSON(path.join(targetDirectory, "id-mapper.json"), Object.fromEntries(idMapperCollection));
+await fs.ensureDir(path.dirname(targetFile));
+fs.writeJSON(targetFile, Object.fromEntries(idMapperCollection));
 console.log(`ID mapper generated with ${idMapperCollection.size} entries.`);
 
