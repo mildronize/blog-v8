0b3e4dda2af571e8c6dd67290b436199ff26a35c
Thada Wangthammang <thada.wth@gmail.com>
2024-12-13 22:33:00 +0700

    feat: write unit test

--- snippets/src/scripts/libs/markdown-files.test.ts
+++ snippets/src/scripts/libs/markdown-files.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from 'bun:test';
-import { processMarkdownDirectories } from './markdown-files';
+import { extractMarkdownMetadata, processMarkdownDirectories } from './markdown-files';
 import { PostId, PostMetadata } from './type';
 
 
@@ -14,4 +14,15 @@ test('processMarkdownFiles', async () => {
   const idMapper = await processMarkdownDirectories(sourceDirs, processor);
 
   expect(idMapper).toEqual(new Map([['id1', { path: 'path1' }]]));
+});
+
+test('extractMarkdownMetadata', () => {
+  const dir = '/content/posts';
+  const markdownFilePath = '/content/posts/2021-09-01-hello-world.md';
+  const content = `+++
+    [extra]
+    id = "id1"
+    +++`;
+  const result = extractMarkdownMetadata(dir, markdownFilePath, content);
+  expect(result).toEqual({ key: 'id1', value: { path: '/posts/hello-world' } });
 });
