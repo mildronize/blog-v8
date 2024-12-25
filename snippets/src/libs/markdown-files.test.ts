fb6ce4e9c8e6b1ffb82e657931a07322e1559311
Thada Wangthammang <thada.wth@gmail.com>
2024-12-25 20:22:01 +0700

    fix(search): fix unit test

--- snippets/src/libs/markdown-files.test.ts
+++ snippets/src/libs/markdown-files.test.ts
@@ -1,19 +1,27 @@
 import { test, expect } from 'bun:test';
 import { extractMarkdownMetadata, processMarkdownDirectories } from './markdown-files';
-import { PostId, IdMapperMetadata, MarkdownMetadata, MarkdownFileProcessorOutput } from './type';
+import { MarkdownFileProcessorOutput } from './type';
 
 
 test('processMarkdownFiles', async () => {
   const sourceDirs = ['test'];
   const processor = {
-    process: async (_dir: string): Promise<MarkdownFileProcessorOutput> => 
-      ({ markdownData: [{ id: 'id1', path: 'path1' }], addedIds: [] }),
-    }
+    process: async (_dir: string): Promise<MarkdownFileProcessorOutput> =>
+    ({
+      markdownData: [{
+        id: 'id1', path: 'path1',
+        frontmatter: { title: 'title1', taxonomies: { tags: ['tag1'], categories: ['category1'] } }, content: 'content1'
+      }], addedIds: []
+    }),
+  }
 
   const idMapper = await processMarkdownDirectories(sourceDirs, processor, console);
 
   expect(idMapper).toEqual({
-    markdownData: [{ id: 'id1', path: 'path1' }],
+    markdownData: [{
+      id: 'id1', path: 'path1',
+      frontmatter: { title: 'title1', taxonomies: { tags: ['tag1'], categories: ['category1'] } }, content: 'content1'
+    }],
     addedIds: [],
   });
 });
@@ -22,12 +30,18 @@ test('extractMarkdownMetadata', () => {
   const dir = '/content/posts';
   const markdownFilePath = '/content/posts/2021-09-01-hello-world.md';
   const content = `+++
+    title = "Hello World"
     [extra]
     id = "id1"
     +++`;
   const result = extractMarkdownMetadata(dir, markdownFilePath, content);
-  expect(result).toEqual({
-    id: 'id1',
-    path: '/posts/hello-world',
-  });
+  expect(result).toStrictEqual(
+
+    {
+      id: 'id1',
+      path: '/posts/hello-world',
+      frontmatter: { extra: { id: 'id1' }, title: 'Hello World' },
+      content: '',
+    }
+  );
 });
