f49dac51d144e9cf956f5cf272844a1cac37cd11
Thada Wangthammang <thada.wth@gmail.com>
2024-12-27 23:47:39 +0700

    feat(search): save state of local checkbox full text search

--- snippets/src/pages/search/App.tsx
+++ snippets/src/pages/search/App.tsx
@@ -2,14 +2,16 @@ import React, { useState, useEffect, createRef } from 'react';
 import './style.css';
 import { BrowserSearch } from '../../libs/search/search-index-broswer';
 
-const apiServer = 'http://localhost:7074';
-
 interface SearchResult {
   field: string[];
   id: string;
   path: string;
   title: string;
   score: number;
+}
+
+export const localStorageKey = {
+  enableFullTextSearch: 'enableFullTextSearch',
 }
 
 const sharedOptions = {
@@ -33,8 +35,6 @@ const browserSearchCollection = {
 // Use the small search index by default
 let browserSearch: BrowserSearch = browserSearchCollection.small;
 
-browserSearch.init();
-
 export default () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState<SearchResult[]>([]);
@@ -52,53 +52,51 @@ export default () => {
     }
   }
 
-  const searchWithApiServer = async (query: string) => {
-    try {
-      const response = await fetch(`${apiServer}/api/search?q=${encodeURIComponent(query)}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch search results');
-      }
-      const data = await response.json();
-      setResults(data.results);
-      setError(null);
-    } catch (err: any) {
-      setError(err.message);
-      setResults([]);
-    }
-  };
-
   useEffect(() => {
+    browserSearch.init();
     // Set query from URL on initial load
     const params = new URLSearchParams(window.location.search);
     const initialQuery = params.get('q') || '';
+    const paramFull = params.get('full');
+    if (paramFull) {
+      setEnableFullTextSearch(paramFull === 'true');
+    }
+    setEnableFullTextSearch(localStorage.getItem(localStorageKey.enableFullTextSearch) === 'true');
     setQuery(initialQuery);
     if (initialQuery) {
       handleSearch(initialQuery);
     }
   }, []);
+  
 
   const handleSearch = (query: string) => {
     if (query) {
-      // searchWithApiServer(query);
       searchOnBrowser(query);
-      // Update the URL with the query parameter
-      const params = new URLSearchParams(window.location.search);
-      params.set('q', query);
-      window.history.replaceState({}, '', `${window.location.pathname}?${params.toString()}`);
+      handleUrlChange(query);
     } else {
       setResults([]);
     }
   };
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    // Do search when Enter key is pressed
-  };
+  const handleUrlChange = (query?: string) => {
+   // If query is not provided, use the current query state
+    if (!query) {
+      query = searchRef.current?.value ?? '';
+    }
+   // Update the URL with the query parameter
+   const params = new URLSearchParams(window.location.search);
+   params.set('action', 'search'); // Define default action
+   params.set('q', query);
+   params.set('full', enableFullTextSearch.toString());
+   window.history.replaceState({}, '', `${window.location.pathname}?${params.toString()}`);
+  }
 
   const handleTextFieldChange = () => {
     const query = searchRef.current?.value ?? '';
     console.log('Search query:', query);
     handleSearch(query);
   };
+
 
   const handleEnableFullTextSearchChange = (value: boolean) => {
     console.log('Enable full text search:', value);
@@ -110,6 +108,7 @@ export default () => {
       browserSearch.init();
     }
     setEnableFullTextSearch(value);
+    localStorage.setItem(localStorageKey.enableFullTextSearch, value.toString());
     handleTextFieldChange();
   }
 
