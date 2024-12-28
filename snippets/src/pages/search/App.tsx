c6ebce76d637d965e87fe7cff1e23d3ac052d359
Thada Wangthammang <thada.wth@gmail.com>
2024-12-28 11:30:04 +0700

    feat(search): improve ui/ux for search field

--- snippets/src/pages/search/App.tsx
+++ snippets/src/pages/search/App.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, createRef } from 'react';
+// import debounce from 'debounce';
 import './style.css';
 import { BrowserSearch } from '../../libs/search/search-index-broswer';
+// import useDebounce from './useDebounce';
 
 interface SearchResult {
   field: string[];
@@ -36,10 +38,13 @@ const browserSearchCollection = {
 let browserSearch: BrowserSearch = browserSearchCollection.small;
 
 export default () => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState<string>('');
   const [results, setResults] = useState<SearchResult[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [enableFullTextSearch, setEnableFullTextSearch] = useState(false);
+  // This state is used to store the debounce timer, prevent multiple search requests
+  // const debouncedQueryValue = useDebounce(query, 300)
+
   const searchRef = createRef<HTMLInputElement>();
 
   const searchOnBrowser = async (query: string) => {
@@ -67,10 +72,12 @@ export default () => {
       handleSearch(initialQuery);
     }
   }, []);
-  
+
+  // const debounceSearch = (query: string) => debounce(() => handleSearch(query), 1000);
 
   const handleSearch = (query: string) => {
     if (query) {
+      console.log('Search query:', query);
       searchOnBrowser(query);
       handleUrlChange(query);
     } else {
@@ -79,28 +86,28 @@ export default () => {
   };
 
   const handleUrlChange = (query?: string) => {
-   // If query is not provided, use the current query state
+    // If query is not provided, use the current query state
     if (!query) {
       query = searchRef.current?.value ?? '';
     }
-   // Update the URL with the query parameter
-   const params = new URLSearchParams(window.location.search);
-   params.set('action', 'search'); // Define default action
-   params.set('q', query);
-   params.set('full', enableFullTextSearch.toString());
-   window.history.replaceState({}, '', `${window.location.pathname}?${params.toString()}`);
+    // Update the URL with the query parameter
+    const params = new URLSearchParams(window.location.search);
+    params.set('action', 'search'); // Define default action
+    params.set('q', query);
+    params.set('full', enableFullTextSearch.toString());
+    window.history.replaceState({}, '', `${window.location.pathname}?${params.toString()}`);
   }
 
-  const handleTextFieldChange = () => {
-    const query = searchRef.current?.value ?? '';
-    console.log('Search query:', query);
+  const handleTextFieldChange = (query: string) => {
+    // const query = searchRef.current?.value ?? '';
+    setQuery(query);
+
     handleSearch(query);
   };
 
-
   const handleEnableFullTextSearchChange = (value: boolean) => {
     console.log('Enable full text search:', value);
-    if(value){
+    if (value) {
       browserSearch = browserSearchCollection.large;
       browserSearch.init();
     } else {
@@ -109,7 +116,16 @@ export default () => {
     }
     setEnableFullTextSearch(value);
     localStorage.setItem(localStorageKey.enableFullTextSearch, value.toString());
-    handleTextFieldChange();
+    handleTextFieldChange(query);
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // When press the ESC key, clear the search box
+    if (e.key === 'Escape') {
+      // searchRef.current?.value = '';
+      setQuery('');
+      setResults([]);
+    }
   }
 
   return (
@@ -118,11 +134,13 @@ export default () => {
         type="text"
         className="search-box"
         placeholder="Search..."
-        defaultValue={query}
+        // defaultValue={query}
+        value={query} // Bind state to the input
         ref={searchRef}
-        onChange={handleTextFieldChange}
+        onKeyDown={(e) => handleKeyDown(e)}
+        onChange={(e) => handleTextFieldChange(e.target.value)}
       />
-       <div className="checkbox-container">
+      <div className="checkbox-container">
         <label className="checkbox-label">
           <input
             type="checkbox"
@@ -137,7 +155,7 @@ export default () => {
         {error && <p className="error">{error}</p>}
         {results.map((result) => (
           <div key={result.id} className="result-item">
-            <a className="result-title" href={result.path}><h3 >{result.title}</h3></a>
+            <a className="result-title" href={result.path}><h4>{result.title}</h4></a>
           </div>
         ))}
       </div>
